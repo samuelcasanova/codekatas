24e787660b4f5b1107492a4c74a7c221331a5723
samuel.casanova <samuel.casanova@immfly.com>
2024-11-05 08:54:37 +0100

    introduce an entity for aged brie with an update() method and a factory class

--- typescript/src/gilded-rose.ts
+++ typescript/src/gilded-rose.ts
@@ -10,28 +10,63 @@ export class Item {
       this.quality = quality;
     }
   }
+
+  export class UpdatableItemFactory {
+    static get(name: string, sellIn: number, quality: number): UpdatableItem {
+      if (name === 'Aged Brie') {
+        return new AgedBrie(name, sellIn, quality);
+      }
+      return new AgedBrie(name, sellIn, quality);
+    }
+  }
   
+  export abstract class UpdatableItem extends Item {
+    abstract update(): void;
+
+    decreaseSellin() {
+      this.sellIn = this.sellIn - SELLIN_STEP;
+    }  
+
+    increaseQuality(qualitySteps = 1) {
+      if (this.quality < MAX_QUALITY) {
+        this.quality = this.quality + qualitySteps * QUALITY_STEP;
+      }
+    }  
+  }
+
+  export class AgedBrie extends UpdatableItem {
+    override update() {
+      this.decreaseSellin();
+
+      const qualitySteps = this.sellIn < MIN_SELLIN ? 2 : 1;
+      this.increaseQuality(qualitySteps);
+    }
+  }
+
   const QUALITY_STEP = 1;
   const NO_QUALITY = 0;
-const MIN_QUALITY = 0;
-const MAX_QUALITY = 50;
-const BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT = 11;
-const BACKSTAGE_PASS_MID_THRESHOLD_LIMIT = 6;
-const MIN_SELLIN = 0;
-const SELLIN_STEP = 1;
+  const MIN_QUALITY = 0;
+  const MAX_QUALITY = 50;
+  const BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT = 11;
+  const BACKSTAGE_PASS_MID_THRESHOLD_LIMIT = 6;
+  const MIN_SELLIN = 0;
+  const SELLIN_STEP = 1;
+
   export class GildedRose {
-    items: Array<Item>;
+    items: Array<UpdatableItem>;
   
-    constructor(items = [] as Array<Item>) {
+    constructor(items = [] as Array<UpdatableItem>) {
       this.items = items;
     }
   
     updateQuality() {
       for(const item of this.items) {
-        if (!isAgedBrie(item) && !isBackstagePass(item)) {
-          if (!isSulfuras(item)) {
+        if (isAgedBrie(item)) {
+          item.update();
+          break;
+        }
+        if (!isAgedBrie(item) && !isBackstagePass(item) && !isSulfuras(item)) {
             decreaseQuality(item);
-          }
         } else {
           increaseQuality(item);
           if (isBackstagePass(item)) {
@@ -47,52 +82,54 @@ const SELLIN_STEP = 1;
           decreaseSellin(item);
         }
         if (item.sellIn < MIN_SELLIN) {
-          if (!isAgedBrie(item)) {
-            if (!isBackstagePass(item)) {
-              if (!isSulfuras(item)) {
-                decreaseQuality(item);
-              }
+          if (!isBackstagePass(item)) {
+            if (isAgedBrie(item) || isSulfuras(item)) {
+              increaseQuality(item);
+            } else {
+              decreaseQuality(item);
+            }
+          } else {
+            if (isAgedBrie(item)) {
+              increaseQuality(item);
             } else {
               dropQuality(item);
             }
-          } else {
-            increaseQuality(item);
           }
         }
       }
 
       return this.items;
+    }
+  }
 
-      function decreaseSellin(item: Item) {
-        item.sellIn = item.sellIn - SELLIN_STEP;
-      }
+  function decreaseSellin(item: Item) {
+    item.sellIn = item.sellIn - SELLIN_STEP;
+  }
 
-      function isBackstagePass(item: Item) {
-        return item.name === 'Backstage passes to a TAFKAL80ETC concert';
-      }
+  function isBackstagePass(item: Item) {
+    return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  }
 
-      function isAgedBrie(item: Item) {
-        return item.name === 'Aged Brie';
-      }
+  function isAgedBrie(item: Item) {
+    return item.name === 'Aged Brie';
+  }
 
-      function isSulfuras(item: Item) {
-        return item.name === 'Sulfuras, Hand of Ragnaros';
-      }
+  function isSulfuras(item: Item) {
+    return item.name === 'Sulfuras, Hand of Ragnaros';
+  }
 
-      function dropQuality(item: Item) {
-        item.quality = NO_QUALITY;
-      }
+  function dropQuality(item: Item) {
+    item.quality = NO_QUALITY;
+  }
 
-      function increaseQuality(item: Item) {
-        if (item.quality < MAX_QUALITY) {
-          item.quality = item.quality + QUALITY_STEP;
-        }
-      }
+  function increaseQuality(item: Item) {
+    if (item.quality < MAX_QUALITY) {
+      item.quality = item.quality + QUALITY_STEP;
+    }
+  }
 
-      function decreaseQuality(item: Item) {
-        if (item.quality > MIN_QUALITY) {
-          item.quality = item.quality - QUALITY_STEP;
-        }
-      }
+  function decreaseQuality(item: Item) {
+    if (item.quality > MIN_QUALITY) {
+      item.quality = item.quality - QUALITY_STEP;
     }
   }
--- typescript/test/gilded-rose.test.ts
+++ typescript/test/gilded-rose.test.ts
@@ -1,64 +1,64 @@
-import { Item, GildedRose } from '../src/gilded-rose';
+import { Item, GildedRose, UpdatableItemFactory } from '../src/gilded-rose';
 
 describe('Gilded Rose', () => {
   describe('When updateQuality()', () => {
     it('should preserve the item name', () => {
-      const gildedRose = new GildedRose([new Item('foo', 10, 10)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('foo', 10, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].name).toBe('foo');
     });
     it('should decrease the sellIn by one day', () => {
-       const gildedRose = new GildedRose([new Item('foo', 10, 10)]);
+       const gildedRose = new GildedRose([UpdatableItemFactory.get('foo', 10, 10)]);
        const items = gildedRose.updateQuality();
        expect(items[0].quality).toBe(9);
     });
     it('should decrease quality by one with regular product', () => {
-        const gildedRose = new GildedRose([new Item('foo', 10, 10)]);
+        const gildedRose = new GildedRose([UpdatableItemFactory.get('foo', 10, 10)]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toBe(9);
     });
     it('should degrade Quality twice as fast once the sell by date has passed', () => {
-      const gildedRose = new GildedRose([new Item('foo', 0, 10)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('foo', 0, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(8);
     });
     it('should degrade Quality unless it reaches zero', () => {
-      const gildedRose = new GildedRose([new Item('foo', 0, 1)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('foo', 0, 1)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(0);
     });
     it('should increase Quality of "Aged Brie" the older it gets', () => {
-      const gildedRose = new GildedRose([new Item('Aged Brie', 3, 1)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('Aged Brie', 3, 1)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(2);
     });
     it('should increase Quality twice as fast of "Aged Brie" the older it gets once the sell by date has passed', () => {
-      const gildedRose = new GildedRose([new Item('Aged Brie', 0, 5)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('Aged Brie', 0, 5)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(7);
     });
     it('should never increase Quality of an item more than 50', () => {
-      const gildedRose = new GildedRose([new Item('Aged Brie', 0, 50)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('Aged Brie', 0, 50)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(50);
     });
     it('should not decrease quality of "Sulfuras"', () => {
-      const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 50)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('Sulfuras, Hand of Ragnaros', 0, 50)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(50);
     });
     it('should increase quality of "Backstage passes" by 1 if there are more than 10 days to the concert', () => {
-      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 10)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('Backstage passes to a TAFKAL80ETC concert', 20, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(11);
     });
     it('should increase quality of "Backstage passes" by 2 if there are X days left to the concert, 10 > X > 5', () => {
-      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('Backstage passes to a TAFKAL80ETC concert', 10, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(12);
     });
     it('should increase quality of "Backstage passes" by 3 if there are less than 5 days left to the concert', () => {
-      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)]);
+      const gildedRose = new GildedRose([UpdatableItemFactory.get('Backstage passes to a TAFKAL80ETC concert', 5, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(13);
     });
