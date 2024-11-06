2867153b9d92be8e095ac0e10158bd22b2dc231e
samuel.casanova <samuel.casanova@immfly.com>
2024-11-06 08:57:54 +0100

    add regular product and final refactor

--- typescript/src/gilded-rose.ts
+++ typescript/src/gilded-rose.ts
@@ -1,149 +1,134 @@
 // https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.md
 export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
-  
-    constructor(name: string, sellIn: number, quality: number) {
-      this.name = name;
-      this.sellIn = sellIn;
-      this.quality = quality;
-    }
-  }
+  name: string;
+  sellIn: number;
+  quality: number;
 
-  export class UpdatableItemFactory {
-    static get(name: string, sellIn: number, quality: number): UpdatableItem {
-      if (name === 'Aged Brie' || name === 'Sulfuras, Hand of Ragnaros') {
-        return new AgingItem(name, sellIn, quality);
-      }
-      if (name === 'Backstage passes to a TAFKAL80ETC concert') {
-        return new BackstagePass(name, sellIn, quality);
-      }
-      return new AgingItem(name, sellIn, quality);
-    }
-  }
-  
-  export abstract class UpdatableItem extends Item {
-    abstract update(): void;
-
-    decreaseSellin() {
-      this.sellIn = this.sellIn - SELLIN_STEP;
-    }  
-
-    increaseQuality(qualitySteps = 1) {
-      if (this.quality < MAX_QUALITY) {
-        this.quality = this.quality + qualitySteps * QUALITY_STEP;
-      }
-    }  
-
-    dropQuality() {
-      this.quality = NO_QUALITY;
-    }  
-  }
-
-  export class AgingItem extends UpdatableItem {
-    override update() {
-      this.decreaseSellin();
-
-      const isOutdated = this.sellIn < MIN_SELLIN;
-      const qualitySteps = isOutdated ? 2 : 1;
-      this.increaseQuality(qualitySteps);
-    }
-  }
-
-export class BackstagePass extends UpdatableItem {
-  override update() {
-    this.increaseQuality();
-    if (this.sellIn < BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT) {
-      this.increaseQuality();
-    }
-    if (this.sellIn < BACKSTAGE_PASS_MID_THRESHOLD_LIMIT) {
-      this.increaseQuality();
-    }
-    this.decreaseSellin();
-    if (this.sellIn < MIN_SELLIN) {
-        this.dropQuality();
-    }
+  constructor(name: string, sellIn: number, quality: number) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
   }
 }
 
-  const QUALITY_STEP = 1;
-  const NO_QUALITY = 0;
-  const MIN_QUALITY = 0;
-  const MAX_QUALITY = 50;
-  const BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT = 11;
-  const BACKSTAGE_PASS_MID_THRESHOLD_LIMIT = 6;
-  const MIN_SELLIN = 0;
-  const SELLIN_STEP = 1;
+export class UpdatableItemFactory {
+  static get(name: string, sellIn: number, quality: number): UpdatableItem {
+    const AGED_BRIE_NAME = 'Aged Brie';
+    const SULFURAS_NAME = 'Sulfuras, Hand of Ragnaros';
+    const BACKSTAGE_PASSES_NAME = 'Backstage passes to a TAFKAL80ETC concert';
 
-  export class GildedRose {
-    items: Array<UpdatableItem>;
-  
-    constructor(items = [] as Array<UpdatableItem>) {
-      this.items = items;
+    if (name === AGED_BRIE_NAME || name === SULFURAS_NAME) {
+      return new AgingItem(name, sellIn, quality);
     }
-  
-    updateQuality() {
-      for(const item of this.items) {
-        if (isAgedBrie(item) || isSulfuras(item) || isBackstagePass(item)) {
-          item.update();
-          break;
-        }
-        if (!isBackstagePass(item)) {
-            decreaseQuality(item);
-        } else {
-          increaseQuality(item);
-          if (isBackstagePass(item)) {
-            if (item.sellIn < BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT) {
-              increaseQuality(item);
-            }
-            if (item.sellIn < BACKSTAGE_PASS_MID_THRESHOLD_LIMIT) {
-              increaseQuality(item);
-            }
-          }
-        }
-        decreaseSellin(item);
-        if (item.sellIn < MIN_SELLIN) {
-          if (!isBackstagePass(item)) {
-            decreaseQuality(item);
-          } else {
-            dropQuality(item);
-          }
-        }
-      }
+    if (name === BACKSTAGE_PASSES_NAME) {
+      return new BackstagePass(name, sellIn, quality);
+    }
+    return new RegularItem(name, sellIn, quality);
+  }
+}
 
-      return this.items;
+const QUALITY_STEP = 1;
+const DEFAULT_QUALITY_STEP_AMOUNT = 1;
+const NO_QUALITY = 0;
+const MIN_QUALITY = 0;
+const MAX_QUALITY = 50;
+const MIN_SELLIN = 0;
+const SELLIN_STEP = 1;
+export abstract class UpdatableItem extends Item {
+  abstract update(): void;
+
+  protected decreaseSellin() {
+    this.sellIn = this.sellIn - SELLIN_STEP;
+  }  
+
+  protected increaseQuality(qualityStepAmount = DEFAULT_QUALITY_STEP_AMOUNT) {
+    if (this.quality < MAX_QUALITY) {
+      this.quality = this.quality + qualityStepAmount * QUALITY_STEP;
+    }
+  }  
+
+  protected dropQuality() {
+    this.quality = NO_QUALITY;
+  }
+
+  protected isOutdated() {
+    return this.sellIn < MIN_SELLIN;
+  }
+
+  protected decreaseQuality() {
+    if (this.quality > MIN_QUALITY) {
+      this.quality = this.quality - DEFAULT_QUALITY_STEP_AMOUNT * QUALITY_STEP;
+    }
+  }  
+}
+
+const AGING_OUTDATED_FACTOR = 2;
+const AGING_IN_SELLIN_FACTOR = 1;
+export class AgingItem extends UpdatableItem {
+  override update() {
+    this.decreaseSellin();
+
+    const qualityStepAmount = this.isOutdated() ? AGING_OUTDATED_FACTOR : AGING_IN_SELLIN_FACTOR;
+    this.increaseQuality(qualityStepAmount * QUALITY_STEP);
+  }
+}
+
+export class RegularItem extends UpdatableItem {
+  override update() {
+    this.decreaseSellin();
+
+    this.decreaseQuality();
+    if (this.sellIn < MIN_SELLIN) {
+      this.decreaseQuality();
+    }      
+  }
+}
+
+const BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT = 11;
+const BACKSTAGE_PASS_LOWER_THRESHOLD_LIMIT = 6;
+const BACKSTAGE_PASSES_UPPER_THRESHOLD_QUALITY_FACTOR = 2;
+const BACKSTAGE_PASSES_LOWER_THRESHOLD_QUALITY_FACTOR = 3;
+export class BackstagePass extends UpdatableItem {
+  override update() {
+    this.decreaseSellin();
+    
+    if (this.isOutdated()) {
+        this.dropQuality();
+    } else if (this.isAboveUpperThreshold()) {
+      this.increaseQuality();
+    } else if (this.isInUpperThreshold()) {
+      this.increaseQuality(BACKSTAGE_PASSES_UPPER_THRESHOLD_QUALITY_FACTOR);
+    } else if (this.isInLowerThreshold()) {
+      this.increaseQuality(BACKSTAGE_PASSES_LOWER_THRESHOLD_QUALITY_FACTOR);
     }
   }
 
-  function decreaseSellin(item: Item) {
-    item.sellIn = item.sellIn - SELLIN_STEP;
+  isAboveUpperThreshold() {
+    return this.sellIn >= BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT
   }
 
-  function isBackstagePass(item: Item) {
-    return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  isInUpperThreshold() {
+    return this.sellIn >= BACKSTAGE_PASS_LOWER_THRESHOLD_LIMIT && this.sellIn < BACKSTAGE_PASS_UPPER_THRESHOLD_LIMIT
   }
 
-  function isAgedBrie(item: Item) {
-    return item.name === 'Aged Brie';
+  isInLowerThreshold() {
+    return this.sellIn > MIN_SELLIN && this.sellIn < BACKSTAGE_PASS_LOWER_THRESHOLD_LIMIT
+  }
+}
+
+export class GildedRose {
+  items: Array<UpdatableItem>;
+
+  constructor(items = [] as Array<UpdatableItem>) {
+    this.items = items;
   }
 
-  function isSulfuras(item: Item) {
-    return item.name === 'Sulfuras, Hand of Ragnaros';
-  }
-
-  function dropQuality(item: Item) {
-    item.quality = NO_QUALITY;
-  }
-
-  function increaseQuality(item: Item) {
-    if (item.quality < MAX_QUALITY) {
-      item.quality = item.quality + QUALITY_STEP;
+  updateQuality() {
+    for(const item of this.items) {
+      item.update();
     }
-  }
 
-  function decreaseQuality(item: Item) {
-    if (item.quality > MIN_QUALITY) {
-      item.quality = item.quality - QUALITY_STEP;
-    }
+    return this.items;
   }
+}
+
